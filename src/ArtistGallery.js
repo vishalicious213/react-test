560e347ba31d1e38a85bcd6073451fdd0152ee50
vishalicious213 <vishalicious213@gmail.com>
2019-11-14 12:10:08 -0500

    added thumbnails & artist name (not overlaid yet) to artist gallery

--- src/ArtistGallery.js
+++ src/ArtistGallery.js
@@ -4,33 +4,25 @@ import './ArtistGallery.css';
 
 const artists = [
   {name: "Leonardo Da Vinci", img: "./img/mona_lisa_by_leonardo_da_vinci.jpg"},
-  {name: "Jan Van Eyck", img: "./img/the_arnolfini_portrait_by_jan_van_eyck.jpg"},
-  {name: "Gustav Klimt", img: "./img/the_kiss_by_gustav_klimt.jpg"},
-  {name: "Rembrandt", img: "./img/the_night_watch_by_rembrandt.jpg"},
   {name: "Vincent Van Gogh", img: "./img/the_starry_night_by_vincent_van_gogh.jpg"},
+  {name: "Rembrandt", img: "./img/the_night_watch_by_rembrandt.jpg"},
+  {name: "Gustav Klimt", img: "./img/the_kiss_by_gustav_klimt.jpg"},
+  {name: "Jan Van Eyck", img: "./img/the_arnolfini_portrait_by_jan_van_eyck.jpg"},
+  {name: "Johannes Vermeer", img: "./img/the_girl_with_a_pearl_earring_by_johannes_vermeer.jpg"},
+  {name: "Claude Monet", img: "./img/impression,_sunrise_by_claude_monet.jpg"},
 ];
 
 function ArtistGallery() {
   return (
     <section className="ArtistGallery">
-      {artists.forEach(artist => {
-        // makeThumbnail(artist);
+      {artists.map(artist => {
+        return (
+          <div className="ArtistGalleryThumbnail" key={artist.name}>
+            <img className="ArtistGalleryImage" src={require(`${artist.img}`)} alt={artist.name}></img>
+            {artist.name}
+          </div>
+        )
       })}
-      <div className="ArtistGalleryThumbnail">
-        image
-      </div>
-      <div className="ArtistGalleryThumbnail">
-        image
-      </div>
-      <div className="ArtistGalleryThumbnail">
-        image
-      </div>
-      <div className="ArtistGalleryThumbnail">
-        image
-      </div>
-      <div className="ArtistGalleryThumbnail">
-        image
-      </div>
     </section>
   );
 }

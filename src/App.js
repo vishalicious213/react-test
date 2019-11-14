a3e99827c269ab1aa0a5d77e3550afbd1998a4bb
vishalicious213 <vishalicious213@gmail.com>
2019-11-14 04:04:03 -0500

    made frame for ArtistGallery component and appended to page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import './App.css';
+import ArtistGallery from "./ArtistGallery"
 // import styled from 'styled-components';
 
 
 function App() {
   return (
-    <section className="top">
-      <nav className="navBar">
-        <i class="navIcon fas fa-home" title="Home"></i>
-        <i class="navIcon fas fa-plus-circle" title="Upload"></i>
-        <i class="navIcon fas fa-sign-in-alt" title="Sign in/Register"></i>
-      </nav>
+    <div className="mainProgram">
+      <section className="top">
+        <nav className="navBar">
+          <i class="navIcon fas fa-home" title="Home"></i>
+          <i class="navIcon fas fa-plus-circle" title="Upload"></i>
+          <i class="navIcon fas fa-sign-in-alt" title="Sign in/Register"></i>
+        </nav>
 
-      <div className="banner">
-        <h1>Art Portfolio</h1>
-      </div>
+        <div className="banner">
+          <h1>Art Portfolio</h1>
+        </div>
 
-      <form className="artistSearchForm">
-        <input className="artistSearchBox" type="text" name="search" placeholder="&#x1f50d; Search artists..." />
-      </form>
-    </section>
+        <form className="artistSearchForm">
+          <input className="artistSearchBox" type="text" name="search" placeholder="&#x1f50d; Search artists..." />
+        </form>
+      </section>
+      
+      <ArtistGallery />
+    </div>
   );
 }
 
---
+++ src/ArtistGallery.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ArtistGallery.css';
+// import styled from 'styled-components';
+
+
+function ArtistGallery() {
+  return (
+    <section className="ArtistGallery">
+      
+    </section>
+  );
+}
+
+export default ArtistGallery;
